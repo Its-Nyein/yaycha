3904efc432f17d4a68fe01a44ddd119e83e0aad7
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-08-27 23:44:06 +0630

    react state action

--- yaycha-client/src/App.jsx
+++ yaycha-client/src/App.jsx
@@ -7,12 +7,15 @@ export default function App() {
     { id: 2, content: "React is fun.", name: "Bob" },
     { id: 3, content: "Yay, interesting.", name: "Chris" },
   ]);
+  const remove = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
   return (
     <div style={{ maxWidth: 600, margin: "20px auto" }}>
       <h1>Yaycha</h1>
       <List>
         {data.map((item) => {
-          return <Item key={item.id} content={item.content} name={item.name} />;
+          return <Item key={item.id} item={item} remove={remove} />;
         })}
       </List>
     </div>
