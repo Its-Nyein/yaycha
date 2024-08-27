13a394b397f211dcf04bd758b81e49afc27f9e52
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-08-28 01:29:20 +0630

    chore: mode switcher

--- yaycha-client/src/ThemeApp.jsx
+++ yaycha-client/src/ThemeApp.jsx
@@ -1,18 +1,22 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useMemo } from "react";
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import App from "./App";
-const theme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
+
 export const AppContext = createContext();
 
 export default function ThemedApp() {
+  const [mode, setMode] = useState("dark");
+
+  const theme = useMemo(() => {
+    return createTheme({
+      palette: { mode },
+    });
+  }, [mode]);
+
   const [showForm, setShowForm] = useState(false);
   return (
     <ThemeProvider theme={theme}>
-      <AppContext.Provider value={{ showForm, setShowForm }}>
+      <AppContext.Provider value={{ showForm, setShowForm, mode, setMode }}>
         <App />
         <CssBaseline />
       </AppContext.Provider>
--- yaycha-client/src/components/Header.jsx
+++ yaycha-client/src/components/Header.jsx
@@ -3,12 +3,13 @@ import {
   Menu as MenuIcon,
   Add as AddIcon,
   LightMode as LightModeIcon,
+  DarkMode as DarkModeIcon,
 } from "@mui/icons-material";
 import { useContext } from "react";
 import { AppContext } from "../ThemeApp";
 
 export default function Header() {
-  const { showForm, setShowForm } = useContext(AppContext);
+  const { showForm, setShowForm, mode, setMode } = useContext(AppContext);
   return (
     <AppBar position="static">
       <Toolbar>
@@ -20,9 +21,23 @@ export default function Header() {
           <IconButton color="inherit" onClick={() => setShowForm(!showForm)}>
             <AddIcon />
           </IconButton>
-          <IconButton color="inherit" edge="end">
-            <LightModeIcon />
-          </IconButton>
+          {mode === "dark" ? (
+            <IconButton
+              color="inherit"
+              edge="end"
+              onClick={() => setMode("light")}
+            >
+              <LightModeIcon />
+            </IconButton>
+          ) : (
+            <IconButton
+              color="inherit"
+              edge="end"
+              onClick={() => setMode("dark")}
+            >
+              <DarkModeIcon />
+            </IconButton>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
