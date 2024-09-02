fa933956c45fe2b9ac880d2f8b4658d9371def6f
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-09-03 01:20:01 +0630

    feat: update PostSeeder.js

---
+++ yaycha-api/prisma/seeds/PostSeeder.js
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+
+const prisma = new PrismaClient();
+
+async function PostSeeder() {
+  const data = [];
+
+  for (let i = 0; i < 20; i++) {
+    const content = faker.lorem.paragraph();
+    const userId = faker.number.int({ min: 1, max: 10 });
+
+    data.push({ content, userId });
+  }
+  console.log("Post seeding started...");
+  await prisma.post.createMany({ data });
+  console.log("Post seeding Done!");
+}
+
+module.exports = { PostSeeder };
