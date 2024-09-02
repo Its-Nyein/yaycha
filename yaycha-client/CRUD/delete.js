0601f6852e80b0004fbe2fa8ae23632e162dc95c
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-09-02 18:42:28 +0630

    chore:fix throw error on delete

--- yaycha-client/CRUD/delete.js
+++ yaycha-client/CRUD/delete.js
@@ -3,15 +3,21 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // thow err because there are many posts that are referenced to User
-prisma.user
-  .delete({
-    where: { id: 1 },
+prisma.post
+  .deleteMany({
+    where: { userId: 1 },
   })
-  .then(() => console.log("Deleted User 1"))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  })
-  .finally(() => {
-    prisma.$disconnect();
+  .then(() => {
+    prisma.user
+      .delete({
+        where: { id: 1 },
+      })
+      .then(() => console.log("Deleted User 1 and their posts"))
+      .catch((err) => {
+        console.error(err);
+        process.exit(1);
+      })
+      .finally(() => {
+        prisma.$disconnect();
+      });
   });
