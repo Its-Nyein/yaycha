331a0216b898937db183ffc9d3cf73f6ff96b664
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-09-03 23:27:32 +0630

    backend API with express

---
+++ yaycha-api/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import { prisma } from "./prismaClient";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/info", (req, res) => {
+  res.json({ msg: "Yaycha API" });
+});
+
+app.listen(8000, () => {
+  console.log("Yaycha API started at port 8000...");
+});
+
+const gracefulShutdown = async () => {
+  await prisma.$disconnect();
+  server.close(() => {
+    console.log("Yaycha API Closed");
+    process.exit(0);
+  });
+};
+
+process.on("SIGTERM", gracefulShutdown);
+process.on("SIGINT", gracefulShutdown);
