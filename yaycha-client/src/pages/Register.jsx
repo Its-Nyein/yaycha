9d33aa1582952c433f64d086f778281097138fba
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-09-10 02:45:04 +0630

    chore: register for new user registration

--- yaycha-client/src/pages/Register.jsx
+++ yaycha-client/src/pages/Register.jsx
@@ -1,18 +1,80 @@
 import { Box, Button, TextField, Typography, Alert } from "@mui/material";
+import { postUser } from "../libs/fetcher";
+import { useContext, useRef, useState } from "react";
+import { AppContext } from "../ThemeApp";
+import { useMutation } from "react-query";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
+  const navigate = useNavigate();
+  const { setGlobalMsg } = useContext(AppContext);
+  const [error, setError] = useState(null);
+
+  const nameInput = useRef();
+  const usernameInput = useRef();
+  const bioInput = useRef();
+  const passwordInput = useRef();
+
+  const handleSubmit = () => {
+    const name = nameInput.current.value;
+    const username = usernameInput.current.value;
+    const bio = bioInput.current.value;
+    const password = passwordInput.current.value;
+
+    if (!name || !username || !password) {
+      setError("Name, username and password required");
+      return false;
+    }
+
+    create.mutate({ name, username, bio, password });
+  };
+
+  const create = useMutation(
+    async (data) => {
+      postUser(data);
+    },
+    {
+      onError: async () => {
+        setError("Cannot create account");
+      },
+      onSuccess: async (user) => {
+        setGlobalMsg("Account created");
+        navigate("/login");
+      },
+    }
+  );
+
   return (
     <Box>
       <Typography variant="h3">Register</Typography>
       <Alert severity="warning" sx={{ mt: 2 }}>
         All fields required
       </Alert>
-      <form>
+      {error && (
+        <Alert severity="warning" sx={{ mt: 2 }}>
+          {error.message}
+        </Alert>
+      )}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
         <Box sx={{ display: "flex", flexDirection: "column", gap: 1, mt: 2 }}>
-          <TextField placeholder="Name" fullWidth />
-          <TextField placeholder="Username" fullWidth />
-          <TextField placeholder="Bio" fullWidth />
-          <TextField type="password" placeholder="Password" fullWidth />
+          <TextField placeholder="Name" fullWidth inputRef={nameInput} />
+          <TextField
+            placeholder="Username"
+            fullWidth
+            inputRef={usernameInput}
+          />
+          <TextField placeholder="Bio" fullWidth inputRef={bioInput} />
+          <TextField
+            type="password"
+            placeholder="Password"
+            fullWidth
+            inputRef={passwordInput}
+          />
           <Button type="submit" variant="contained" fullWidth>
             Register
           </Button>
