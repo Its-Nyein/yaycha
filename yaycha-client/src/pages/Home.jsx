edddab72da199b308bd2fee17e324b2a0b5a0474
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-11-09 23:32:05 +0630

    chore: set loading and errors

--- yaycha-client/src/pages/Home.jsx
+++ yaycha-client/src/pages/Home.jsx
@@ -1,19 +1,30 @@
 import { useEffect, useState } from "react";
 import Item from "../components/Item";
 import Form from "../components/Form";
-import { Box, Container } from "@mui/material";
+import { Alert, Box, Container } from "@mui/material";
 import { useApp } from "../ThemeApp";
 
 export default function Home() {
   const { showForm, setGlobalMsg } = useApp();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const api = import.meta.env.VITE_API;
-    fetch(`${api}/content/posts`).then(async (res) => {
-      setData(await res.json());
-    });
+    fetch(`${api}/content/posts`)
+      .then(async (res) => {
+        if (res.ok) {
+          setData(await res.json());
+          setLoading(false);
+        } else {
+          setError(true);
+        }
+      })
+      .catch(() => {
+        setError(true);
+      });
   }, []);
 
   const add = (content, name) => {
@@ -26,6 +37,18 @@ export default function Home() {
     setData(data.filter((d) => d.id !== id));
     setGlobalMsg("An Item Deleted.");
   };
+
+  if (error) {
+    return (
+      <Box>
+        <Alert severity="warning">Cannot fetch data</Alert>
+      </Box>
+    );
+  }
+
+  if (loading) {
+    return <Box sx={{ textAlign: "center" }}>Loading...</Box>;
+  }
 
   return (
     <Box>
